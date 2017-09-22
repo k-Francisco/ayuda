4b57a5379906c136e0b0e34eb795769c814511ae
k-francisco <piattosnovalays@gmail.com>
2017-09-22 16:13:00 +0800

    half of the layouts is done

---
+++ app/src/main/java/com/charapp/charapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.charapp.charapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activities_card_layout);
+    }
+}
